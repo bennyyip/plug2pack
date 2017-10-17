83248aa27192439555a5a87b2a441ade5fc1b8fa
BennyYip <yebenmy@protonmail.com>
2017-10-17 20:38:23 +0800

    v0.1.0

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,11 @@
 use std::str;
 use std::collections::HashMap;
+use nom::IResult;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Value {
-  Str(String),
-  Array(Vec<String>),
+    Str(String),
+    Array(Vec<String>),
 }
 
 named!(string <&str>,
@@ -34,7 +35,7 @@ ws!(
   )
 );
 
-named!(hash <HashMap<String, Value>>,
+named!(hash <HashMap<&str, Value>>,
        ws!(
          map!(
            delimited!(
@@ -44,9 +45,9 @@ named!(hash <HashMap<String, Value>>,
              )
            ,
            |tuple_vec| {
-             let mut h: HashMap<String, Value> = HashMap::new();
+             let mut h: HashMap<&str, Value> = HashMap::new();
              for (k, v) in tuple_vec {
-               h.insert(String::from(k), v);
+               h.insert(k, v);
              }
              h
            }
@@ -63,13 +64,48 @@ named!(value<Value>,
          )
       );
 
-named!(pub parse<(&str, HashMap<String,Value>)>,
-       ws!(
-         do_parse!(
-           tag!("plug")
-           >> name: string
-           >> options: hash
-           >> (name,options)
-           )
-         )
-      );
+named!(parse_plug<(&str, Option<HashMap<&str,Value>>)>,
+ws!(
+  do_parse!(
+    tag!("Plug")
+    >> name: string
+    >> options: alt!(
+      eof!()                    => {|_| None} |
+      preceded!(tag!(","),hash) => {|h| Some(h)}
+      )
+    >> (name,options)
+    )
+  )
+);
+
+pub fn parse(content: &[u8]) -> Option<(&str, Option<HashMap<&str, Value>>)> {
+    match parse_plug(content) {
+        IResult::Done(_, r) => Some(r),
+        _ => None,
+    }
+}
+
+#[test]
+fn parse_works() {
+
+
+    assert_eq!(IResult::Done(&b""[..], "foo"), string(&b"'foo'"[..]));
+
+    assert_eq!(IResult::Done(&b""[..], Value::Str("foo".to_owned())), value(&b"'foo'"[..]));
+
+    assert_eq!(IResult::Done(&b""[..],("for",Value::Str("python".to_owned()))),key_value(&b"'for':'python' "[..]));
+
+    let s = &b" { 'for': 'python' }";
+    let mut h = HashMap::new();
+    h.entry("for").or_insert(Value::Str("python".to_owned()));
+
+    assert_eq!(IResult::Done(&b""[..],h.clone()),hash(&s[..]));
+
+    let s = &b"Plug 'davidhalter/jedi-vim', { 'for': 'python' }";
+
+    assert_eq!(IResult::Done(&b""[..], ("davidhalter/jedi-vim",Some(h.clone()))), parse_plug(&s[..]) );
+
+    let s = &b"Plug 'davidhalter/jedi-vim'";
+
+    assert_eq!(IResult::Done(&b""[..], ("davidhalter/jedi-vim",None)), parse_plug(&s[..]) );
+}
---
+++ test/foo.vim
@@ -0,0 +1,11 @@
+Plug 'davidhalter/jedi-vim', { 'for': 'python' }
+Plug 'bennyyip/vim-yapf', { 'for': 'python' }
+
+Plug 'PProvost/vim-ps1'
+Plug 'ekalinin/Dockerfile.vim'
+
+Plug 'derekwyatt/vim-scala', { 'for': 'scala' }
+
+Plug 'Rykka/riv.vim', { 'for': 'rst' }
+Plug 'amix/vim-zenroom2', { 'for': [ 'markdown', 'rst', 'txt'] }
+Plug 'lervag/vimtex', {'for': 'tex'}
